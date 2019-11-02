bd18a037a7d8dec6d4cbb52be8cde27f4d850eef
Annie Huang <annie.hn@gmail.com>
2019-11-02 20:37:25 +1100

    Remove func-names from the list.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-const x = 'Hello';
+app.get('/', function(req, res) {
+  res.send('hello');
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
