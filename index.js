2b0543521378cbf5956b473784d5b92a7365a1b9
Annie Huang <annie.hn@gmail.com>
2019-11-02 20:57:36 +1100

    Add "class-methods-use-this" into the list

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
 
-const obj = {
-  hello: function() {       // It prefers the format of hello() {
-    console.log('hello');
+// const obj = {
+//   hello: function() {       // It prefers the format of hello() {
+//     console.log('hello');
+//   }
+// };
+
+class A {
+  foo() {
+    console.log('Hello World');
+    /* error Expected 'this' to be used by class method 'foo'. */
   }
-};
+}
