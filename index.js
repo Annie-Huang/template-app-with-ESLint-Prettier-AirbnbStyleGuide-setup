bbf20b1209ec699849747dbcebbc38fb9db58c2a
Annie Huang <annie.hn@gmail.com>
2019-11-02 19:04:15 +1100

    Now I add prettier/prettier, it keep complaining "  8:3  error    Delete `··`                             prettier/prettier". Have to keep tab === 2 white spaces

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const x = 'Hello';
+
 app.listen(3000, () => {
-    console.log('App listening on port 3000!')
+  console.log('App listening on port 3000!');
 });
